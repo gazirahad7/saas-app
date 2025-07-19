f27aa8986bea189ae8974fc7ca8761b84e779e8d
Azizul Islam Rahad <gazi.rahad871@gmail.com>
2025-07-19 14:44:02 +0600

    Fix: this  theme switcher hydration issue

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,23 @@
 "use client";
 
-import * as React from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  // solution start
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  // solution end
+
+  //main theme return
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
